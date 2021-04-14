3f991a3d6476ec89dc5934da710923cf85c16207
Sophie Kunz <skn@Sophies-MacBook-Pro.local>
2021-04-14 17:52:25 +0200

    changing simpleDebug for getVerifiedITBNr

--- src/calvingDataMap.cpp
+++ src/calvingDataMap.cpp
@@ -578,27 +578,27 @@ string calvingDataMap::getVerifiedITBNr(string itb, string indstr){
     simpleDebug("getVerifiedITBNr()_Setting itb to missing, because ITB_NR_LENGTH is not correct", indstr);
     return CONSTANTS::STRING_NA;
   }else{
-    simpleDebug("getVerifiedITBNr()_Plausible itb "+itb+" considering ITB_NR_LENGTH ", indstr);
+    simpleDebug("getVerifiedITBNr()_Plausible itb considering ITB_NR_LENGTH ", indstr);
   }
 
   // check whether first six positions of id are letters
   string breedCountrySexCode = itb.substr(0,CONSTANTS::BREED_COUNTRY_SEX_CODE_LENGTH);
   // countryCode should not be numeric
   if (strspn(breedCountrySexCode.c_str(), CONSTANTS::LETTERS) != CONSTANTS::BREED_COUNTRY_SEX_CODE_LENGTH){
-    simpleDebug("getVerifiedITBNr()_Setting itb to missing, because first part (breed/country/sex) are not letters", indstr);
+    simpleDebug("getVerifiedITBNr()_Setting itb to missing, because first part (breed/country/sex) "+breedCountrySexCode+" are not letters", indstr);
     return CONSTANTS::STRING_NA;
   }else{
-    simpleDebug("getVerifiedITBNr()_Plausible itb "+itb+" considering first part (breed/country/sex)", indstr);
+    simpleDebug("getVerifiedITBNr()_Plausible itb considering first part (breed/country/sex)", indstr);
   }
 
   // check whether second parts is only numeric
   string aniMM = itb.substr(CONSTANTS::BREED_COUNTRY_SEX_CODE_LENGTH);
   // aniMM must be all numbers
   if (strspn(aniMM.c_str(), CONSTANTS::NUMBERS) != (CONSTANTS::ITB_NR_LENGTH - CONSTANTS::BREED_COUNTRY_SEX_CODE_LENGTH)){
-    simpleDebug("getVerifiedITBNr()_Setting itb to missing, because numeric part is not numeric", indstr);
+    simpleDebug("getVerifiedITBNr()_Setting itb to missing, because numeric part "+aniMM+" is not numeric", indstr);
     return CONSTANTS::STRING_NA;
   }else{
-    simpleDebug("getVerifiedITBNr()_Plausible itb "+itb+" considering numeric part", indstr);
+    simpleDebug("getVerifiedITBNr()_Plausible itb considering numeric part", indstr);
   }
 
   return itb;
