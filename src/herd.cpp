435bf496af30a27e0eb027575301f4aade27d022
Sophie Kunz <skn@Sophies-MacBook-Pro.local>
2021-05-19 12:09:27 +0200

    only calving ease as trait to define in increment

--- src/herd.cpp
+++ src/herd.cpp
@@ -12,19 +12,18 @@ herd::herd(){}
 void herd::increment(int transformedCalvingScoreInt){
 
   // trait
-  string traitBW = "bw";
   string traitCE = "ce";
 
   // statistic per herd
-  // birthweight
-  map<string,statistic*> :: iterator bit = HerdStatistic.find(traitBW);
+  // calving ease
+  map<string,statistic*> :: iterator bit = HerdStatistic.find(traitCE);
   if(bit == HerdStatistic.end()){
     statistic *sPtr = new statistic();
     sPtr->ObsPerHerd = 1;
-    sPtr->trait = traitBW;
+    sPtr->trait = traitCE;
     sPtr->Quadriert += (double) transformedCalvingScoreInt * (double) transformedCalvingScoreInt;
     sPtr->Sum += (double) transformedCalvingScoreInt;
-    HerdStatistic[traitBW] = sPtr;
+    HerdStatistic[traitCE] = sPtr;
   }else{
     bit->second->ObsPerHerd++;
     bit->second->Quadriert += (double) transformedCalvingScoreInt * (double) transformedCalvingScoreInt;
