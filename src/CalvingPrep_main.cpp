b25ca35eec852084a96c55a6b4c76a01427cacd0
Sophie Kunz <skn@Sophies-MacBook-Pro.local>
2021-10-08 11:34:02 +0200

    Start check Debugging file in inputCalvingData

--- src/CalvingPrep_main.cpp
+++ src/CalvingPrep_main.cpp
@@ -76,13 +76,13 @@ int CalvingPrep_main(std::string paramFileName) {
   }
 
 
-//  //Read raw calving-data and check the field of the data-record
-//  for(unsigned i=0; i<numberDataFiles; i++){
-//    string fileName = cMap.getFileName(i,dataFile);
-//    cMap.inputCalvingData(fileName, aMap, lastYearToConsiderData, parSampling, startYearSampling, endYearSampling,\
-//                          parselectSplitMandantBeef, parselectSplitMandantDairy);
-//  }
-//
+  //Read raw calving-data and check the field of the data-record
+  for(unsigned i=0; i<numberDataFiles; i++){
+    string fileName = cMap.getFileName(i,dataFile);
+    cMap.inputCalvingData(fileName, aMap, lastYearToConsiderData, parSampling, startYearSampling, endYearSampling,\
+                          parselectSplitMandantBeef, parselectSplitMandantDairy);
+  }
+
 //  // Minimum observations per breedcombination
 //  cMap.countBreedComb();
 //  cMap.purgeBreedComb();
