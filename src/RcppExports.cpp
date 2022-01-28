89a6c7a0630139de90c15261663ab1d34fa7fd6a
Sophie Kunz <sophie.kunz@qualitasag.ch>
2022-01-28 11:29:32 +0100

    install change of only read 1 file

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // CalvingPrep_main
 int CalvingPrep_main(std::string paramFileName);
 RcppExport SEXP _CalvingPrep_CalvingPrep_main(SEXP paramFileNameSEXP) {
